d6beaca081af1d0e8899d15d8f48780b3305fb8d
OssLM <osslmrg@outlook.com>
2025-02-14 02:32:17 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,22 @@
-const urlSearchParams = new URLSearchParams(window.location.search)
-
-const messageCustom = urlSearchParams.get('message')
-
-if (messageCustom) {
-
-  const mainMessageElement = document.querySelector('#mainMessage')
-  mainMessageElement.textContent = decodeURI(messageCustom)
-}
-
-// the tutorial starts here
-
-const btnOpenElement = document.querySelector('#open')
-const btnCloseElement = document.querySelector('#close')
-
-btnCloseElement.disabled = true
-
-
-
-btnOpenElement.addEventListener('click', ()=> {
-  btnOpenElement.disabled = true
-  btnCloseElement.disabled = false
-  const coverElement = document.querySelector('.cover')
-  coverElement.classList.add('open-cover')
-
-  setTimeout(()=>{
-    //
-    coverElement.style.zIndex = -1
+$(document).ready(function(){
+      // Al cargar la página, ocultamos las cortinas
+      $('.left-curtain').css('width', '0%');
+      $('.right-curtain').css('width', '0%');
     
-    const paperElement = document.querySelector('.paper')
-    paperElement.classList.remove('close-paper')
-    paperElement.classList.add('open-paper')
-
-    // animacion del corazón
-    const heartElement = document.querySelector('.heart')
-    heartElement.style.display = 'block'
-  
-  }, 500)
-
-})
-btnCloseElement.addEventListener('click', ()=> {
-  btnOpenElement.disabled = false
-  btnCloseElement.disabled = true
-
-  const coverElement = document.querySelector('.cover')
-  const paperElement = document.querySelector('.paper')
-  paperElement.classList.remove('open-paper')
-  paperElement.classList.add('close-paper')
-  
-  setTimeout(()=>{
-    coverElement.style.zIndex = 0
-    coverElement.classList.remove('open-cover')
-
-    // animacion del corazón
-    const heartElement = document.querySelector('.heart')
-    heartElement.style.display = 'none'
-  },500)
-})
+      $('.valentines-day').click(function(){
+        // Animación de desvanecimiento de los elementos del sobre
+        $('.envelope').css({'animation':'fall 3s linear 1', '-webkit-animation':'fall 3s linear 1'});
+        $('.envelope').fadeOut(800, function() {
+          // Ocultar elementos dentro de .valentines-day
+          $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
+          
+    
+          // Hacer visible la carta con una animación ondulante
+          $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
+          $('#card').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
+            var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
+            $(this).css('transform', 'scale(' + scale + ')');
+          }}); // Animación de ondulación
+        });
+      });
+    });
